af314048dfe7684574a39a39506eb290448152cd
Abhyantaadya IT Consulting Services <abhyantaadya.itcs@gmail.com>
2025-07-15 07:59:01 +0530

    Increase logo size by 1.5 times

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -65,7 +65,7 @@ const Header = () => {
                   src="/Adobe Express - file (1).png" 
                   alt="Action Guarding" 
                   className={`w-auto transition-all duration-500 ${
-                    isHomePage && scrollProgress > 0.3 ? 'h-7 lg:h-9' : 'h-8 lg:h-10'
+                    isHomePage && scrollProgress > 0.3 ? 'h-10 lg:h-14' : 'h-12 lg:h-15'
                   }`}
                 />
                 <span className={`font-montserrat font-bold text-white hover:text-agspl-red transition-all duration-500 ${
